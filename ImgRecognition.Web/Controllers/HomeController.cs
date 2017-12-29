d8a4e9a10f0ca6f43189ab2d90515ea834dc39e6
EMEA\smetatat <Zaq1@wsx>
2017-12-29 15:47:05 +0300

    no message

--- ImgRecognition.Web/Controllers/HomeController.cs
+++ ImgRecognition.Web/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using ImgRecognition.BusinessLogic.DataServices.Implementation;
 using System.IO;
+using System.Web;
 using System.Web.Mvc;
 
 namespace ImgRecognition.Web.Controllers
@@ -7,18 +8,41 @@ namespace ImgRecognition.Web.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
-            var fileName = "C:\\Work\\_personal\\ImgRecognition\\ImgRecognition.Web\\Samples\\coala.jpg";
-
-            byte[] bytes = System.IO.File.ReadAllBytes(fileName);
-            Stream stream = new MemoryStream(bytes);
-
-            var imgRecognition = new CognitiveImgRecognition();
-            var result = imgRecognition.RecognizeImage(stream);
-
-            string s = result.Result.Description;
-
+        {           
             return View();
         }
+
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+                    string _FileName = Path.GetFileName(file.FileName);
+                    string _path = Path.Combine(Server.MapPath("~/Samples"), _FileName);
+                    file.SaveAs(_path);
+
+                    byte[] bytes = System.IO.File.ReadAllBytes(_path);
+                    Stream stream = new MemoryStream(bytes);
+
+                    var imgRecognition = new CognitiveImgRecognition();
+                    var result = imgRecognition.RecognizeImage(stream);
+
+                    ViewBag.Img = _path;
+                    ViewBag.Description = result.Result.Description;
+                    ViewBag.Tags = result.Result.Tags;
+                }
+
+                ViewBag.Message = "File Uploaded Successfully!!";
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "File upload failed!!";
+                return View();
+            }
+        }
+
     }
 }
